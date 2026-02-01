f6aaded420b0c175d94355be1e7cc9112aa94420
Abhijnan Dikshit <abhijnan2000@gmail.com>
2026-02-01 18:36:49 -0500

    updated setup file

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="scimlstudio",
-    version="1.0.0",
+    version="1.3.0",
     packages=find_packages(include=["scimlstudio*"]),
     python_requires=">=3.12",
     install_requires=[
         "torch>=2.9.0",
+        "gpytorch>=1.15.1",
+        "botorch>=0.16.1"
     ]
 )
