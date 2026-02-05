7d50bdc0bf8dc0e87c5e65a8daa3a9773f372204
Abhijnan Dikshit <abhijnan2000@gmail.com>
2026-02-04 20:46:21 -0500

    updated setup to match latest release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="scimlstudio",
-    version="1.3.0",
+    version="1.4.0",
     packages=find_packages(include=["scimlstudio*"]),
     python_requires=">=3.12",
     install_requires=[
