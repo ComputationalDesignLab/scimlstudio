9db520cb0d12e8693a1e77ce008509140d9bd776
Abhijnan Dikshit <abhijnan2000@gmail.com>
2026-03-25 00:04:03 -0400

    bumped package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="scimlstudio",
-    version="1.6.3",
+    version="1.7.1",
     packages=find_packages(include=["scimlstudio*"]),
     python_requires=">=3.12",
     install_requires=[
